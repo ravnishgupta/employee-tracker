75fe707ed15f1b6b88eb9fc51aa293c7f6460df8
ravnishgupta <ravnish@gmail.com>
2022-04-23 20:03:04 -0700

    db, schema, seed

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO department (name) values ('FINANCE');
+INSERT INTO department (name) values ('LEGAL');
+INSERT INTO department (name) values ('ENGINEERING');
+
+INSERT INTO role (title, salary, department_id) values ('SENIOR ACCOUNTANT', 80000, 1);
+INSERT INTO role (title, salary, department_id) values ('FRONT-END ENGINEER', 120000, 3);
+INSERT INTO role (title, salary, department_id) values ('LEGAL COUNSEL', 120000, 2);
+
+INSERT INTO employee (first_name, last_name, role_id) values ('JOHN', 'SMITH', 1);
+INSERT INTO employee (first_name, last_name, role_id) values ('WILL', 'SMITH', 2);
+INSERT INTO employee (first_name, last_name, role_id) values ('OSCAR', 'WILDE', 3);
