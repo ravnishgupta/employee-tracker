8f7ae6782508418794226b8e8367939b6c7f831b
ravnishgupta <ravnish@gmail.com>
2022-04-26 16:57:38 -0700

    ability to add a role

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const inquirer = require('inquirer');
 const {addDepartment, getAllDepartments} = require('./lib/department')
 const {getAllRoles, addRole} = require('./lib/role')
-const {getAllEmployees} = require('./lib/employee');
+const {getAllEmployees,getAllManagers} = require('./lib/employee');
 const db = require('./db/connection')
 const cTable = require('console.table')
+
 
 //view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
 
@@ -57,7 +58,7 @@ const consumeSelection = (selection) => {
                 )
             break;
             case "VIEW ALL DEPARTMENTS":
-                var ret = ''
+                let ret = ''
                 getAllDepartments(function(result){
                     ret = result;
                     console.log(cTable.getTable(ret));
@@ -65,29 +66,29 @@ const consumeSelection = (selection) => {
                 })
                break;
             case "VIEW ALL ROLES":
-                var ret = ''
+                let roles = ''
                 getAllRoles(function(result){
-                    ret = result;
-                    console.log(cTable.getTable(ret));
+                    roles = result;
+                    console.log(cTable.getTable(roles));
                     askQuestions();
                 })
                break;
             case "VIEW ALL EMPLOYEES":
-                var ret = ''
+                let emp = ''
                 getAllEmployees(function(result){
-                    ret = result;
-                    console.log(cTable.getTable(ret));
+                    emp = result;
+                    console.log(cTable.getTable(emp));
                     askQuestions();
                 })
-               break;
-        case "ADD A ROLE":
-            let deptArray = [];
-            getAllDepartments(function(result){
-                ret = result
-                ret.forEach((department) => {deptArray.push(department.DEPARTMENT_NAME);});
-                //console.log(deptArray)
-            })
-            inquirer.prompt([
+            break;
+            case "ADD A ROLE":
+                let dept = ''
+                let deptArray = [];
+                getAllDepartments(function(result){
+                    dept = result
+                    dept.forEach((department) => {deptArray.push(department.DEPARTMENT_NAME);});
+                })
+                inquirer.prompt([
                 {
                     type: 'input',
                     name: 'roleTitle',
@@ -123,30 +124,96 @@ const consumeSelection = (selection) => {
                     choices: deptArray
                 }
 
-            ])
-            .then (data => { 
-                let departmentId;
+                ])
+                .then (data => { 
+                    let departmentId;
 
-                ret.forEach((department) => {
-                    //console.log(department.DEPARTMENT_NAME, data.roleDept)
-                    if (department.DEPARTMENT_NAME === data.roleDept) {departmentId = department.DEPARTMENT_ID;}
-                });
+                    dept.forEach((department) => {
+                        if (department.DEPARTMENT_NAME === data.roleDept) {departmentId = department.DEPARTMENT_ID;}
+                    });
 
-                let rtn;
-                addRole(data.roleTitle, data.salary, departmentId, function(result){
-                    rtn = result;
-                    if (result) {
-                        console.log(`${data.roleTitle.toUpperCase()} successfully added`)
-                        askQuestions()
+                    let rtn;
+                    addRole(data.roleTitle, data.salary, departmentId, function(result){
+                        rtn = result;
+                        if (result) {
+                            console.log(`${data.roleTitle.toUpperCase()} successfully added`)
+                            askQuestions()
+                        }
+                        else console.log('fail')
+                    })            
+                })
+            break;
+            case "ADD AN EMPLOYEE":
+                let mgrArray = [];
+                let mgr =''
+                getAllManagers(function(result){
+                    mgr = result
+                    mgr.forEach((manager) => mgrArray.push(`${manager.first_name} ${manager.last_name}`));
+                })
+                
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'firstName',
+                        message: "What is the employee's first name? (Required)",
+                        validate: firstName => {
+                            if (firstName) {
+                                return true;
+                            }
+                            else {
+                                console.log('First name is required.');
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        type: 'input',
+                        name: 'lastName',
+                        message: "What is the employee's last name? (Required)",
+                        validate: lastName => {
+                            if (lastName) {
+                                return true;
+                            }
+                            else {
+                                console.log('Last name is required.');
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        type: 'confirm',
+                        name: 'hasManager',
+                        message: "Does this employee have a manager? (Required)"
                     }
-                    else console.log('fail')
-                })            
-            })
+                ])
+                .then((data) => {
+                    
+                    if (data.hasManager) {
+                        inquirer.prompt([
+                            {
+                                type: 'rawlist',
+                                name: 'empMgr',
+                                message: 'Please select a Manager.',
+                                choices: mgrArray
+                            }
+                        ])
+                        .then (data =>  {
+                                            let mgrID;
+                                            mgr.forEach((manager) => {
+                                            if ((manager.first_name === data.empMgr.split(' ')[0]) && ((manager.last_name === data.empMgr.split(' ')[1]))) {
+                                                mgrID = manager.id;
+                                            }
+                                        })
+                        });
+                        
+                    }
+                    else {
+                        
+                    }
+                })
+                break;
 
 
     }
 }
-
-
-
 askQuestions()
